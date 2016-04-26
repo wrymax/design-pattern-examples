ef99dab0b82f52a05a261551beba66e501a21f70
王若愚 <wrymax@qq.com>
2016-04-26 21:52:23 +0800

    add Decorator

--- Decorator/Java/Main.java
+++ Decorator/Java/Main.java
@@ -8,3 +8,94 @@
  */
 
 // TODO..
+interface ICoffee {
+	void showCoffee();
+
+	float showPrice();
+}
+
+//原始咖啡
+class Coffee implements ICoffee {
+	private String name;
+	private float price;
+
+	public Coffee(String name, float price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	@Override
+	public void showCoffee() {
+		System.out.println(name + " coffee");
+	}
+
+	@Override
+	public float showPrice() {
+		return price;
+	}
+}
+
+//抽象装饰器
+abstract class Decorator implements ICoffee {
+	private ICoffee coffee;
+
+	public void setCoffee(ICoffee coffee) {
+		this.coffee = coffee;
+	}
+
+	@Override
+	public void showCoffee() {
+		coffee.showCoffee();
+	}
+
+	@Override
+	public float showPrice() {
+		return coffee.showPrice();
+	}
+}
+
+//加糖咖啡
+class Sugar extends Decorator {
+	@Override
+	public void showCoffee() {
+		System.out.println("加糖");
+		super.showCoffee();
+	}
+
+	@Override
+	public float showPrice() {
+		return super.showPrice() + 5;
+	}
+}
+
+//补全,加牛奶的咖啡
+class SugarMilk extends Decorator {
+	@Override
+	public void showCoffee() {
+		System.out.println("加牛奶");
+		super.showCoffee();
+	}
+
+	@Override
+	public float showPrice() {
+		return super.showPrice() + 10;
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		Coffee coffee = new Coffee("拿铁", 20);
+		//加糖
+		Decorator sugar = new Sugar();
+		sugar.setCoffee(coffee);
+		System.out.println(sugar.showPrice());
+		
+		//补全,加糖,加牛奶的咖啡
+		Decorator sugerMilk = new SugarMilk();
+		sugerMilk.setCoffee(coffee);
+		System.out.println(sugerMilk.showPrice());
+	}
+
+}
+
